539603e46fefc6de158f3bb3b77954929d8c9338
P. Gilbert Schmitt <PGilbertSchmitt@gmail.com>
2024-12-23 10:12:11 -0500

    readonly type

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ export type ValueOf<T extends Record<string, unknown>> = T[keyof T];
 
 export type Grid<T> = T[][];
 
-export type Vec = [number, number];
+export type Vec = readonly [number, number];
 
 export const Dirs = {
   N: 0,
